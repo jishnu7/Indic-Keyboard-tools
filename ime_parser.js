bc4535262293a12561cc8852f6a11db1f5517181
Jishnu Mohan <jishnu7@gmail.com>
2014-05-25 10:58:00 +0530

    wire directly to file, print debug data to console.

--- ime_parser.js
+++ ime_parser.js
@@ -1,9 +1,10 @@
 // Author: Jishnu Mohan <jishnu7@gmail.com>
 
 var http = require('http'),
-  async = require('async');
+  async = require('async'),
+  fs = require('fs'),
+  file = process.argv[2];
 
-var file = process.argv[2];
 if(!file) {
   return console.log("Error: Pass file name as parameter");
 }
@@ -12,13 +13,12 @@ var sort = [
   'Q','W','E','R','T','Y','U','I','O','P','\\{','\\}','\\|',
   'q','w','e','r','t','y','u','i','o','p','\\[','\\]','\\\\',
   'A','S','D','F','G','H','J','K','L','\\:', '\"',null,null,
-  'a','s','d','f','g','h','j','k','l','\\;', '\'',null,null,
-  'Z','X','C','V','B','N','M','\\<','\\>','?',null,null,null,
+  'a','s','d','f','g','h','j','k','l',';', '\'',null,null,
+  'Z','X','C','V','B','N','M','\\<','\\>','\\?',null,null,null,
   'z','x','c','v','b','n','m',',','.','/',null,null,null,
   '`','1','2','3','4','5','6','7','8','9','0','\\-','\\=',
-  '~','!','\\@','\\#','\\$','\\%','\\^','\\&','\\*','\\(','\\)','_','\\+'
+  '~','\\!','\\@','\\#','\\$','\\%','\\^','\\&','\\*','\\(','\\)','_','\\+'
 ];
-
 var toHex = function(char) {
   var hex = char.charCodeAt(0).toString(16).toUpperCase();
   if(char.length < 4) {
@@ -28,12 +28,11 @@ var toHex = function(char) {
 };
 
 var charDetails = function(char, callback) {
-  var query = {
+  var query = JSON.stringify({
       method:  "chardetails.getdetails",
       params: [char],
       id: ""
-    };
-  query = JSON.stringify(query);
+    });
 
   var req = http.request({
     host: "silpa.org.in",
@@ -54,6 +53,7 @@ var charDetails = function(char, callback) {
     });
     res.addListener('end', function() {
       var val=" ";
+      console.log(data);
       try {
         val = JSON.parse(data);
         val = val.result[char].Name;
@@ -69,7 +69,8 @@ jQuery = {
     register: function(data) {
       var out = [],
         extra = [],
-        calls = [];
+        calls = [],
+        count = 0;
 
       data.patterns.forEach(function(key) {
         if(key[1] === "") {
@@ -80,6 +81,8 @@ jQuery = {
             pos = key[0],
             index = sort.indexOf(pos);
 
+          console.log('---------------------------');
+          console.log(++count, pos, char);
           charDetails(char, function(name) {
             if(index !== -1) {
               out[sort.indexOf(pos)] = '\t\t\t<!--' + pos + ' ' + char + ' ' + name + '-->\n' +
@@ -94,13 +97,18 @@ jQuery = {
         });
       });
 
-      async.parallel(calls, function(err, results) {
-        out.forEach(function(line) {
-          console.log(line.replace(/\t/g, "    "));
+      async.series(calls, function(err, results) {
+        var stream = fs.createWriteStream(file.split('.')[0] + '.xml');
+        stream.once('open', function(fd) {
+          out.forEach(function(line) {
+            stream.write(line.replace(/\t/g, "    "));
+          });
+          stream.write("\n\n\n");
+          extra.forEach(function(line) {
+            stream.write(line.replace(/\t/g, "    "));
+          });
+          stream.end();
         });
-        if(extra.length > 0) {
-          console.log("\n\n\n", extra);
-        }
       });
     }
   }
