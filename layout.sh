bdc7a53c556924daa8330b8c78ea160b062736fb
Jishnu Mohan <jishnu7@gmail.com>
2014-05-27 11:32:06 +0530

    generalizing src file to copy

--- layout.sh
+++ layout.sh
@@ -1,6 +1,7 @@
 # Author: Jishnu Mohan <jishnu7@gmail.com>
 # 04-03-2014
 location="/home/jishnu/git/IndicKeyboard"
+src="kashmiri"
 
 if [ $# -lt 1 ]
 then
@@ -9,21 +10,21 @@ then
 fi
 case "$2" in
     inscript)
-        files=('kbd_kannada_inscript.xml' 'keyboard_layout_set_kannada_inscript.xml'
-            'rows_kannada_inscript.xml'
-            'rowkeys_kannada_inscript1.xml' 'rowkeys_kannada_inscript2.xml'
-            'rowkeys_kannada_inscript3.xml' 'rowkeys_kannada_inscript4.xml')
+        files=('kbd_'$src'_inscript.xml' 'keyboard_layout_set_'$src'_inscript.xml'
+            'rows_'$src'_inscript.xml'
+            'rowkeys_'$src'_inscript1.xml' 'rowkeys_'$src'_inscript2.xml'
+            'rowkeys_'$src'_inscript3.xml' 'rowkeys_'$src'_inscript4.xml')
         ;;
     *)
-        files=('kbd_kannada.xml' 'keyboard_layout_set_kannada.xml'
-            'rows_kannada.xml' 'rowkeys_kannada1.xml'
-            'rowkeys_kannada2.xml' 'rowkeys_kannada3.xml')
+        files=('kbd_$src.xml' 'keyboard_layout_set_$src.xml'
+            'rows_$src.xml' 'rowkeys_$src1.xml'
+            'rowkeys_$src2.xml' 'rowkeys_$src3.xml')
         ;;
 esac
 
 cd $location/java/res/xml/
 for i in "${files[@]}"
 do
-    echo "copying... ${i/kannada/$1}"
-    sed "s/kannada/${1}/g" $i > ${i/kannada/$1}
+    echo "copying... ${i/$src/$1}"
+    sed "s/$src/${1}/g" $i > ${i/$src/$1}
 done
